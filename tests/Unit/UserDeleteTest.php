7e7fcdbaf71e19660e249cab1d2809e100b1c94e
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-10-31 14:21:38 -0400

    Feature: Created a unit test to delete a user in the database

---
+++ tests/Unit/UserDeleteTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UserDeleteTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUserDeleteTest()
+    {
+        $user = User::all()->find(3);
+        $user->delete();
+        $this->assertDatabaseMissing('users', ['name' => $user->name]);
+    }
+}
