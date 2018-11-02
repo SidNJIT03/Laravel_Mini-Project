e21a4da46dad742c5b7ec3dd40ef5cd2eaad3d2e
Siddharth Santhanakrishnan <ss3875@njit.edu>
2018-11-02 14:35:12 -0400

    Fix: Made the drop-down menu work for logout by adding js script

--- resources/views/pages/thankyou.blade.php
+++ resources/views/pages/thankyou.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Thank You {{$name}}</h1>
+    <h1>Thank You For Submitting your message {{$name}}. We will get back to you as soon as possible.</h1>
 @endsection
